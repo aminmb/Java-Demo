99fd1e996dee48d4cdce06d9828925e770794348
aminmb <amin.mohammadbagheri@gmail.com>
2016-12-26 23:59:53 -0600

    Some modification

--- demo/ArrayProcessingDemo/src/amin/ArrayProcessing.java
+++ demo/ArrayProcessingDemo/src/amin/ArrayProcessing.java
@@ -7,7 +7,7 @@ public class ArrayProcessing {
 	public static void main(String[] args) {
 		System.out.println("\nExample 1, Print the value by index");
 		int[] A1 = { 2, 8, 4, 3, 1, 9, 0, 7, 5 };
-		System.out.println("The element is " + A1[5]);
+		System.out.println("The value of the 5th element is " + A1[5]);
 
 		
 		// Print the Array Values
@@ -17,12 +17,13 @@ public class ArrayProcessing {
 		}
 		
 		// create an array with random values
-		System.out.println("\nExample 3, create an array with random values. (From 1 to 10.)");
+		System.out.println("\nExample 3, create an array with random values. (Between 1 and 10.)");
 		double[] A2 = new double [10];
 		for (int i=0; i<A2.length; i++){
 			A2[i]= Math.floor (Math.random()*10);
-			System.out.println (i + " th index of A2 is " + A2[i]);
 		}
+		System.out.println("The random Array is " + Arrays.toString(A2));
+
 		
 		// find the maximum
 		System.out.println("\nExample 4, Find the maximum between elements and print the index");
@@ -57,8 +58,6 @@ public class ArrayProcessing {
 		double[] reversedArray = new double[size];
 		for (int i=0; i<size; i++){
 			reversedArray[i] = A6[size-1-i];
-			System.out.println("index is " + i + " "+ size + " "+ reversedArray[i] + " "+ A6[i]);
-			//System.out.println("The " + i + "th value of the reversed array is "+ reversedArray[i]);	
 		}
 		System.out.println("The reversed Array is " + Arrays.toString(reversedArray));
 	}
