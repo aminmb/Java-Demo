ffcf2df415ce1eeeff9594206db2a760384cda79
aminmb <amin.mohammadbagheri@gmail.com>
2017-01-02 03:10:27 -0600

    Change the variable name from guess to rootSquare

--- JavaExamples/src/example/SquareNewton.java
+++ JavaExamples/src/example/SquareNewton.java
@@ -4,15 +4,15 @@ public class SquareNewton {
 		
 		public static void main(String[] args){
 			
-			double guess = 1.0;
+			double rootSquare = 1.0;
 			double epsilon = 0.001;
 			double x= 2.0;
 			
-			while (Math.abs(guess*guess - x) > epsilon){
-				guess = (x/guess +guess) /2;
+			while (Math.abs(rootSquare*rootSquare - x) > epsilon){
+				rootSquare = (x/rootSquare + rootSquare) /2;
 				
 			}
 			
-			System.out.println(guess);
+			System.out.println(rootSquare);
 		}
 }
